8ed008c20802f42df2122f6f0e397ed5f2cc3e5b
HyCraftHD <hycrafthd@live.de>
2018-11-05 16:56:42 +0100

    First test worked correctly

--- src/main/java/info/u_team/useful_railroads/proxy/CommonProxy.java
+++ src/main/java/info/u_team/useful_railroads/proxy/CommonProxy.java
@@ -1,13 +1,16 @@
 package info.u_team.useful_railroads.proxy;
 
+import info.u_team.useful_railroads.init.*;
 import net.minecraftforge.fml.common.event.*;
 
 public class CommonProxy {
 	
 	public void preinit(FMLPreInitializationEvent event) {
+		UsefulRailroadsBlocks.preinit();
 	}
 	
 	public void init(FMLInitializationEvent event) {
+		UsefulRailroadsCreativeTabs.init();
 	}
 	
 	public void postinit(FMLPostInitializationEvent event) {
