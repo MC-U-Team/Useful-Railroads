1f7e05ef2c0d77758a3a3e80862fbf7993e2b583
HyCraftHD <hycrafthd@live.de>
2019-08-13 03:14:28 +0200

    Add container open

--- src/main/java/info/u_team/useful_railroads/block/TeleportRailBlock.java
+++ src/main/java/info/u_team/useful_railroads/block/TeleportRailBlock.java
@@ -3,13 +3,20 @@ package info.u_team.useful_railroads.block;
 import info.u_team.useful_railroads.init.UsefulRailroadsTileEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.item.minecart.AbstractMinecartEntity;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 
 public class TeleportRailBlock extends CustomTileEntityPoweredRailBlock {
 	
 	public TeleportRailBlock(String name) {
 		super(name, () -> UsefulRailroadsTileEntities.TELEPORT_RAIL);
+	}
+	
+	@Override
+	public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+		return openContainer(world, pos, player, true);
 	}
 	
 	@Override
