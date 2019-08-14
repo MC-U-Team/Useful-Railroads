5811017e0a2caf4bdbab239f941c0e65fc3a14ac
HyCraftHD <hycrafthd@live.de>
2019-08-14 19:52:14 +0200

    Remove config completly

--- src/main/java/info/u_team/useful_railroads/proxy/CommonProxy.java
+++ src/main/java/info/u_team/useful_railroads/proxy/CommonProxy.java
@@ -1,15 +1,11 @@
 package info.u_team.useful_railroads.proxy;
 
 import info.u_team.u_team_core.api.IModProxy;
-import info.u_team.useful_railroads.config.CommonConfig;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 public class CommonProxy implements IModProxy {
 	
 	@Override
 	public void construct() {
-		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
 	}
 	
 	@Override
