89f07d7ce75dc47650d7a6d41d25d270b162e6eb
HyCraftHD <hycrafthd@live.de>
2019-09-16 16:53:18 +0200

    Add function to prevent placement of rails if the location is not setuped yet

--- src/main/java/info/u_team/useful_railroads/item/TeleportRailBlockItem.java
+++ src/main/java/info/u_team/useful_railroads/item/TeleportRailBlockItem.java
@@ -2,7 +2,11 @@ package info.u_team.useful_railroads.item;
 
 import info.u_team.useful_railroads.block.TeleportRailBlock;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.text.*;
 
 public class TeleportRailBlockItem extends BlockItem {
 	
@@ -11,8 +15,20 @@ public class TeleportRailBlockItem extends BlockItem {
 	}
 	
 	@Override
+	public ActionResultType tryPlace(BlockItemUseContext context) {
+		final CompoundNBT compound = context.getItem().getChildTag("BlockEntityTag");
+		if (compound != null && compound.contains("location")) {
+			return super.tryPlace(context);
+		}
+		final PlayerEntity player = context.getPlayer();
+		if (player != null) {
+			player.sendMessage(new StringTextComponent(TextFormatting.DARK_RED + "You need to setup the rail first."));
+		}
+		return ActionResultType.FAIL;
+	}
+	
+	@Override
 	public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
-		// stack.getChildTag("BlockEntityTag")
 		return false;
 	}
 	
