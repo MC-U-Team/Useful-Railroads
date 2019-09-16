11cee88d8bc6691ea5dd6c8652ea7ae987cf4a9d
HyCraftHD <hycrafthd@live.de>
2019-09-16 13:29:31 +0200

    Fix display icon in item group to be a rail and not a placeholder

--- src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsItemGroups.java
+++ src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsItemGroups.java
@@ -2,10 +2,9 @@ package info.u_team.useful_railroads.init;
 
 import info.u_team.u_team_core.itemgroup.UItemGroup;
 import info.u_team.useful_railroads.UsefulRailroadsMod;
-import net.minecraft.item.Items;
 
 public class UsefulRailroadsItemGroups {
 	
-	public static final UItemGroup GROUP = new UItemGroup(UsefulRailroadsMod.MODID, "group", () -> Items.ACACIA_BOAT);
+	public static final UItemGroup GROUP = new UItemGroup(UsefulRailroadsMod.MODID, "group", () -> UsefulRailroadsBlocks.HIGHSPEED_RAIL);
 	
 }
