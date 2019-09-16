4d7ca4513003a145a0c85e921538aa3a781d4497
HyCraftHD <hycrafthd@live.de>
2019-09-16 17:33:59 +0200

    Port setup code from 1.12 with a a better aproach and use lambdas and streams

--- src/main/java/info/u_team/useful_railroads/item/TeleportRailBlockItem.java
+++ src/main/java/info/u_team/useful_railroads/item/TeleportRailBlockItem.java
@@ -1,11 +1,14 @@
 package info.u_team.useful_railroads.item;
 
 import info.u_team.useful_railroads.block.TeleportRailBlock;
+import info.u_team.useful_railroads.util.Location;
+import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.text.*;
 import net.minecraft.world.World;
 
@@ -29,18 +32,32 @@ public class TeleportRailBlockItem extends BlockItem {
 	}
 	
 	@Override
-	public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
+	public boolean onEntityItemUpdate(ItemStack stack, ItemEntity itemEntity) {
 		final CompoundNBT compound = stack.getChildTag("BlockEntityTag");
 		if (compound != null && compound.contains("location")) { // Prevent overwriting already installed rails
 			return false;
 		}
-		final World world = entity.getEntityWorld();
+		final World world = itemEntity.getEntityWorld();
 		if (world.isRemote) {
 			// Do client things
 		} else {
-			
+			final int age = itemEntity.age;
+			if (age < 100) {
+				return false;
+			}
+			final AxisAlignedBB aabb = new AxisAlignedBB(itemEntity.posX - 1, itemEntity.posY - 1, itemEntity.posZ - 1, itemEntity.posX + 1, itemEntity.posY + 1, itemEntity.posZ + 1);
+			world.getEntitiesInAABBexcluding(itemEntity, aabb, ItemEntity.class::isInstance).stream() //
+					.map(ItemEntity.class::cast) //
+					.map(ItemEntity::getItem) //
+					.filter(otherStack -> otherStack.getItem() == Items.ENDER_PEARL) //
+					.findAny() //
+					.ifPresent(otherStack -> {
+						stack.shrink(1);
+						compound.put("location", new Location(world.getDimension().getType(), itemEntity.getPosition()).serializeNBT());
+						// itemEntity.setItem(stack);
+						world.addEntity(new LightningBoltEntity(world, itemEntity.posX, itemEntity.posY, itemEntity.posZ, true));
+					});
 		}
 		return false;
 	}
-	
 }
