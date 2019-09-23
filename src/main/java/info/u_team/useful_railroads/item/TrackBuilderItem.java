75f54409bbfdbba87dc3fa303338d19684b270cf
HyCraftHD <hycrafthd@live.de>
2019-09-23 22:29:30 +0200

    Some refactoring

--- src/main/java/info/u_team/useful_railroads/item/TrackBuilderItem.java
+++ src/main/java/info/u_team/useful_railroads/item/TrackBuilderItem.java
@@ -51,33 +51,13 @@ public class TrackBuilderItem extends UItem {
 		if (player == null || !context.isPlacerSneaking()) { // No player or no sneaking
 			return ActionResultType.PASS;
 		}
-		final ItemStack stack = context.getItem();
-		final TrackBuilderInventoryWrapper wrapper = new TrackBuilderInventoryWrapper.Server(stack, () -> player.world);
-		
 		final TrackBuilderManager manager = new TrackBuilderManager(context.getPos(), context.getFace(), world, player.getLookVec());
 		
 		if (!manager.calculateBlockPosition()) {
 			return ActionResultType.PASS;
 		}
 		
-		manager.execute(player, wrapper);
-		
-//		manager.getAllPositionsSet().forEach(pos -> {
-//			BlockState previous = world.getBlockState(pos);
-//			world.setBlockState(pos, Blocks.DIAMOND_ORE.getDefaultState());
-//			world.markAndNotifyBlock(pos, world.getChunkAt(pos), Blocks.AIR.getDefaultState(), Blocks.DIAMOND_ORE.getDefaultState(), 3);
-//		});
-		
-		// Short[] objectArray = manager.getRenderSet().stream() //
-		// .map(pos -> new Vec3i(pos.getX() & 15, pos.getY(), pos.getZ() & 15)) //
-		// .map(vec -> (short) (vec.getX() << 12 | vec.getZ() << 8 | vec.getY())) //
-		// .toArray(Short[]::new);
-		//
-		// short[] array = new short[objectArray.length];
-		// IntStream.range(0, array.length).forEach(i -> array[i] = objectArray[i]);
-		//
-		// ((ServerPlayerEntity) player).connection.sendPacket(new SMultiBlockChangePacket(objectArray.length, array,
-		// world.getChunkAt(player.getPosition())));
+		manager.execute(player, new TrackBuilderInventoryWrapper.Server(context.getItem(), () -> player.world));
 		
 		return super.onItemUse(context);
 	}
