85029679e8b1d61edfcda256cea1aa4d30715c4d
HyCraftHD <hycrafthd@live.de>
2020-02-07 01:28:05 +0100

    Add checker

--- src/main/java/info/u_team/useful_railroads/UsefulRailroadsMod.java
+++ src/main/java/info/u_team/useful_railroads/UsefulRailroadsMod.java
@@ -1,10 +1,15 @@
 package info.u_team.useful_railroads;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(UsefulRailroadsMod.MODID)
 public class UsefulRailroadsMod {
 	
 	public static final String MODID = "usefulrailroads";
+	
+	public UsefulRailroadsMod() {
+		JarSignVerifier.checkSigned(MODID);
+	}
 	
 }
