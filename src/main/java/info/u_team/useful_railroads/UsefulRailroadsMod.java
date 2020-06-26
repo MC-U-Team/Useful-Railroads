4008582958c2c3ff6e4baefb59593bde2646d8d6
HyCraftHD <hycrafthd@live.de>
2020-06-26 19:03:24 +0200

    Fix the container types

--- src/main/java/info/u_team/useful_railroads/UsefulRailroadsMod.java
+++ src/main/java/info/u_team/useful_railroads/UsefulRailroadsMod.java
@@ -3,7 +3,7 @@ package info.u_team.useful_railroads;
 import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.useful_railroads.config.UsefulRailroadsConfig;
-import info.u_team.useful_railroads.init.UsefulRailroadsBlocks;
+import info.u_team.useful_railroads.init.*;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -21,6 +21,7 @@ public class UsefulRailroadsMod {
 	
 	private void register() {
 		BusRegister.registerMod(UsefulRailroadsBlocks::register);
+		BusRegister.registerMod(UsefulRailroadsContainerTypes::register);
 	}
 	
 }
--- src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsContainerTypes.java
+++ src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsContainerTypes.java
@@ -1,25 +1,24 @@
 package info.u_team.useful_railroads.init;
 
 import info.u_team.u_team_core.containertype.UContainerType;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import info.u_team.useful_railroads.UsefulRailroadsMod;
 import info.u_team.useful_railroads.container.*;
 import net.minecraft.inventory.container.ContainerType;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@EventBusSubscriber(modid = UsefulRailroadsMod.MODID, bus = Bus.MOD)
 public class UsefulRailroadsContainerTypes {
 	
-	public static final ContainerType<TeleportRailContainer> TELEPORT_RAIL = new UContainerType<>("teleport_rail", TeleportRailContainer::new);
+	public static final CommonDeferredRegister<ContainerType<?>> CONTAINER_TYPES = CommonDeferredRegister.create(ForgeRegistries.CONTAINERS, UsefulRailroadsMod.MODID);
 	
-	public static final ContainerType<TrackBuilderContainer> TRACK_BUILDER = new UContainerType<>("track_builder", TrackBuilderContainer::new);
+	public static final RegistryObject<ContainerType<TeleportRailContainer>> TELEPORT_RAIL = CONTAINER_TYPES.register("teleport_rail", () -> new UContainerType<>(TeleportRailContainer::new));
 	
-	@SubscribeEvent
-	public static void register(Register<ContainerType<?>> event) {
-		BaseRegistryUtil.getAllGenericRegistryEntriesAndApplyNames(UsefulRailroadsMod.MODID, ContainerType.class).forEach(event.getRegistry()::register);
+	public static final RegistryObject<ContainerType<TrackBuilderContainer>> TRACK_BUILDER = CONTAINER_TYPES.register("track_builder", () -> new UContainerType<>(TrackBuilderContainer::new));
+	
+	public static void register(IEventBus bus) {
+		CONTAINER_TYPES.register(bus);
 	}
 	
 }
