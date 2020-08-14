82dbafa4e234cd6f34fa1a505035fc4e93db846e
HyCraftHD <hycrafthd@live.de>
2020-08-14 17:25:45 +0200

    Use ItemSlot as implementation now

--- src/main/java/info/u_team/useful_railroads/inventory/CustomTextureItemSlotHandler.java
+++ src/main/java/info/u_team/useful_railroads/inventory/CustomTextureItemSlotHandler.java
@@ -1,9 +1,10 @@
 package info.u_team.useful_railroads.inventory;
 
+import info.u_team.u_team_core.container.ItemSlot;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.items.*;
+import net.minecraftforge.items.IItemHandler;
 
-public class CustomTextureItemSlotHandler extends SlotItemHandler {
+public class CustomTextureItemSlotHandler extends ItemSlot {
 	
 	public CustomTextureItemSlotHandler(IItemHandler itemHandler, ResourceLocation atlasLocation, ResourceLocation location, int index, int xPosition, int yPosition) {
 		super(itemHandler, index, xPosition, yPosition);
