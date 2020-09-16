2d3a71eb3557a8928f653a77b2781418689697a2
HyCraftHD <hycrafthd@live.de>
2020-09-16 20:20:26 +0200

    Add the new stuff in the ctor

--- src/main/java/info/u_team/useful_railroads/UsefulRailroadsMod.java
+++ src/main/java/info/u_team/useful_railroads/UsefulRailroadsMod.java
@@ -1,17 +1,19 @@
 package info.u_team.useful_railroads;
 
-import info.u_team.u_team_core.util.registry.BusRegister;
+import info.u_team.u_team_core.util.annotation.AnnotationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import info.u_team.useful_railroads.config.*;
-import info.u_team.useful_railroads.init.*;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(UsefulRailroadsMod.MODID)
 public class UsefulRailroadsMod {
 	
 	public static final String MODID = "usefulrailroads";
+	
+	public UsefulRailroadsMod() {
+		JarSignVerifier.checkSigned(MODID);
+		
+		AnnotationManager.callAnnotations(MODID);
+	}
 	
 //	public UsefulRailroadsMod() {
 //		JarSignVerifier.checkSigned(MODID);
