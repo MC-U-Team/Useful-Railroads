ec4abc892fdc86a4625a73f269b3e1b2f662ecac
HyCraftHD <hycrafthd@live.de>
2020-09-17 16:11:35 +0200

    Use the right namespace for our recipe types

--- src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsRecipeTypes.java
+++ src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsRecipeTypes.java
@@ -1,12 +1,13 @@
 package info.u_team.useful_railroads.init;
 
+import info.u_team.useful_railroads.UsefulRailroadsMod;
 import info.u_team.useful_railroads.recipe.*;
 import net.minecraft.item.crafting.IRecipeType;
 
 public class UsefulRailroadsRecipeTypes {
 	
-	public static IRecipeType<TeleportRailFuelRecipe> TELEPORT_RAIL_FUEL = IRecipeType.register("teleport_rail_fuel");
+	public static IRecipeType<TeleportRailFuelRecipe> TELEPORT_RAIL_FUEL = IRecipeType.register(UsefulRailroadsMod.MODID + ":teleport_rail_fuel");
 	
-	public static IRecipeType<TrackBuilderFuelRecipe> TRACK_BUILDER_FUEL = IRecipeType.register("track_builder_fuel");
+	public static IRecipeType<TrackBuilderFuelRecipe> TRACK_BUILDER_FUEL = IRecipeType.register(UsefulRailroadsMod.MODID + ":track_builder_fuel");
 	
 }
