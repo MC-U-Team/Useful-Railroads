6dd9ba5adc169e1f0766e48865dd582dd2cd3c54
HyCraftHD <hycrafthd@live.de>
2020-09-17 15:40:31 +0200

    Add useful railroads common construct

---
+++ src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsCommonConstruct.java
@@ -0,0 +1,12 @@
+package info.u_team.useful_railroads.init;
+
+import info.u_team.u_team_core.api.construct.*;
+import info.u_team.useful_railroads.UsefulRailroadsMod;
+
+@Construct(modid = UsefulRailroadsMod.MODID)
+public class UsefulRailroadsCommonConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+	}
+}
