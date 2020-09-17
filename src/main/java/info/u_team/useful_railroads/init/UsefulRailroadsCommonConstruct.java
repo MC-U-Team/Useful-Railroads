f9a4445b8998db5829557530c4c54b3c2e8ee127
HyCraftHD <hycrafthd@live.de>
2020-09-17 15:41:29 +0200

    Register configs

--- src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsCommonConstruct.java
+++ src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsCommonConstruct.java
@@ -2,11 +2,16 @@ package info.u_team.useful_railroads.init;
 
 import info.u_team.u_team_core.api.construct.*;
 import info.u_team.useful_railroads.UsefulRailroadsMod;
+import info.u_team.useful_railroads.config.*;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Construct(modid = UsefulRailroadsMod.MODID)
 public class UsefulRailroadsCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
+		ModLoadingContext.get().registerConfig(Type.SERVER, ServerConfig.CONFIG);
 	}
 }
