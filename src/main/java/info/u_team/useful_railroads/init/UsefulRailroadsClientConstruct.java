500b65cb47fe4acf7ee475ff15612c51021e3bdc
HyCraftHD <hycrafthd@live.de>
2020-11-06 15:12:07 +0100

    Fix bus registry

--- src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsClientConstruct.java
+++ src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsClientConstruct.java
@@ -14,6 +14,6 @@ public class UsefulRailroadsClientConstruct implements IModConstruct {
 		BusRegister.registerMod(UsefulRailroadsRenderTypes::registerMod);
 		BusRegister.registerMod(UsefulRailroadsScreens::registerMod);
 		
-		BusRegister.registerMod(DrawTrackBuilderSelectionEventHandler::registerForge);
+		BusRegister.registerForge(DrawTrackBuilderSelectionEventHandler::registerForge);
 	}
 }
