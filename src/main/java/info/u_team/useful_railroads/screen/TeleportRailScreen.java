fe17582505f43b2c0fae43b2758c359753b22449
HyCraftHD <hycrafthd@live.de>
2020-11-06 14:46:03 +0100

    Use the text components for rendering now

--- src/main/java/info/u_team/useful_railroads/screen/TeleportRailScreen.java
+++ src/main/java/info/u_team/useful_railroads/screen/TeleportRailScreen.java
@@ -7,7 +7,6 @@ import info.u_team.useful_railroads.UsefulRailroadsMod;
 import info.u_team.useful_railroads.container.TeleportRailContainer;
 import info.u_team.useful_railroads.tileentity.TeleportRailTileEntity;
 import info.u_team.useful_railroads.util.Location;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -19,8 +18,26 @@ public class TeleportRailScreen extends UBasicContainerScreen<TeleportRailContai
 	
 	private static final ResourceLocation BACKGROUND = new ResourceLocation(UsefulRailroadsMod.MODID, "textures/gui/teleport_rail.png");
 	
+	private final ITextComponent dimensionTextComponent;
+	private final ITextComponent xTextComponent;
+	private final ITextComponent yTextComponent;
+	private final ITextComponent zTextComponent;
+	private final ITextComponent fuelTextComponent;
+	private final ITextComponent consumptionTextComponent;
+	private final ITextComponent seperatorTextComponent;
+	
 	public TeleportRailScreen(TeleportRailContainer container, PlayerInventory playerInventory, ITextComponent title) {
 		super(container, playerInventory, title, BACKGROUND, 176, 189);
+		
+		final String langKey = "container.usefulrailroads.teleport_rail.";
+		
+		dimensionTextComponent = new TranslationTextComponent(langKey + "dimension");
+		xTextComponent = new TranslationTextComponent(langKey + "x");
+		yTextComponent = new TranslationTextComponent(langKey + "y");
+		zTextComponent = new TranslationTextComponent(langKey + "z");
+		fuelTextComponent = new TranslationTextComponent(langKey + "fuel");
+		consumptionTextComponent = new TranslationTextComponent(langKey + "consumption");
+		seperatorTextComponent = new StringTextComponent(": ");
 	}
 	
 	@Override
@@ -31,15 +48,13 @@ public class TeleportRailScreen extends UBasicContainerScreen<TeleportRailContai
 		final Location location = tileEntity.getLocation();
 		final BlockPos pos = location.getPos();
 		
-		final String langKey = "container.usefulrailroads.teleport_rail.";
+		font.func_243248_b(matrixStack, dimensionTextComponent.copyRaw().append(seperatorTextComponent).append(new StringTextComponent(location.getRegistryKey().getLocation().toString()).mergeStyle(TextFormatting.DARK_GREEN)), 11, 23, 0x404040);
+		font.func_243248_b(matrixStack, xTextComponent.copyRaw().append(seperatorTextComponent).append(new StringTextComponent(Integer.toString(pos.getX())).mergeStyle(TextFormatting.DARK_GREEN)), 11, 33, 0x404040);
+		font.func_243248_b(matrixStack, yTextComponent.copyRaw().append(seperatorTextComponent).append(new StringTextComponent(Integer.toString(pos.getY())).mergeStyle(TextFormatting.DARK_GREEN)), 11, 43, 0x404040);
+		font.func_243248_b(matrixStack, zTextComponent.copyRaw().append(seperatorTextComponent).append(new StringTextComponent(Integer.toString(pos.getZ())).mergeStyle(TextFormatting.DARK_GREEN)), 11, 53, 0x404040);
 		
-		font.drawString(matrixStack, I18n.format(langKey + "dimension") + ": " + TextFormatting.DARK_GREEN + location.getRegistryKey().getLocation(), 11, 23, 0x404040);
-		font.drawString(matrixStack, I18n.format(langKey + "x") + ": " + TextFormatting.DARK_GREEN + pos.getX(), 11, 33, 0x404040);
-		font.drawString(matrixStack, I18n.format(langKey + "y") + ": " + TextFormatting.DARK_GREEN + pos.getY(), 11, 43, 0x404040);
-		font.drawString(matrixStack, I18n.format(langKey + "z") + ": " + TextFormatting.DARK_GREEN + pos.getZ(), 11, 53, 0x404040);
-		
-		font.drawString(matrixStack, I18n.format(langKey + "fuel") + ": " + TextFormatting.DARK_AQUA + tileEntity.getFuel(), 11, 68, 0x404040);
-		font.drawString(matrixStack, I18n.format(langKey + "consumption") + ": " + TextFormatting.DARK_RED + tileEntity.getCost(), 11, 78, 0x404040);
+		font.func_243248_b(matrixStack, fuelTextComponent.copyRaw().append(seperatorTextComponent).append(new StringTextComponent(Integer.toString(tileEntity.getFuel())).mergeStyle(TextFormatting.DARK_AQUA)), 11, 68, 0x404040);
+		font.func_243248_b(matrixStack, consumptionTextComponent.copyRaw().append(seperatorTextComponent).append(new StringTextComponent(Integer.toString(tileEntity.getCost())).mergeStyle(TextFormatting.DARK_RED)), 11, 78, 0x404040);
 	}
 	
 }
