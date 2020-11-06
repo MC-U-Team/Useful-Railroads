d98fa52c96883221e0a9ddf9c65d132528cf2f43
HyCraftHD <hycrafthd@live.de>
2020-11-06 14:32:34 +0100

    Use text components at the start now. Should increase fps in guis a bit

--- src/main/java/info/u_team/useful_railroads/screen/TrackBuilderScreen.java
+++ src/main/java/info/u_team/useful_railroads/screen/TrackBuilderScreen.java
@@ -6,7 +6,6 @@ import info.u_team.u_team_core.gui.elements.BetterButton;
 import info.u_team.u_team_core.screen.UBasicContainerScreen;
 import info.u_team.useful_railroads.UsefulRailroadsMod;
 import info.u_team.useful_railroads.container.TrackBuilderContainer;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.*;
@@ -17,10 +16,28 @@ public class TrackBuilderScreen extends UBasicContainerScreen<TrackBuilderContai
 	
 	private static final ResourceLocation BACKGROUND = new ResourceLocation(UsefulRailroadsMod.MODID, "textures/gui/track_builder.png");
 	
+	private final ITextComponent modeTextComponent;
+	private final ITextComponent railsTextComponent;
+	private final ITextComponent groundBlocksTextComponent;
+	private final ITextComponent tunnelBlocksTextComponent;
+	private final ITextComponent redstoneTorchesTextComponent;
+	private final ITextComponent torchesTextComponent;
+	private final ITextComponent fuelTextComponent;
+	
 	public TrackBuilderScreen(TrackBuilderContainer container, PlayerInventory playerInventory, ITextComponent title) {
 		super(container, playerInventory, title, BACKGROUND, 284, 296);
 		backgroundWidth = backgroundHeight = 512;
 		setTextLocation(8, 6, 62, ySize - 94);
+		
+		final String langKey = "container.usefulrailroads.track_builder.";
+		
+		modeTextComponent = new TranslationTextComponent(langKey + "mode");
+		railsTextComponent = new TranslationTextComponent(langKey + "rails");
+		groundBlocksTextComponent = new TranslationTextComponent(langKey + "ground_blocks");
+		tunnelBlocksTextComponent = new TranslationTextComponent(langKey + "tunnel_blocks");
+		redstoneTorchesTextComponent = new TranslationTextComponent(langKey + "redstone_torches");
+		torchesTextComponent = new TranslationTextComponent(langKey + "torches");
+		fuelTextComponent = new TranslationTextComponent(langKey + "fuel");
 	}
 	
 	@Override
@@ -42,19 +59,17 @@ public class TrackBuilderScreen extends UBasicContainerScreen<TrackBuilderContai
 	protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int mouseX, int mouseY) {
 		super.drawGuiContainerForegroundLayer(matrixStack, mouseX, mouseY);
 		
-		final String langKey = "container.usefulrailroads.track_builder.";
+		font.func_243248_b(matrixStack, modeTextComponent, 169, 6, 0x404040);
 		
-		font.func_243248_b(matrixStack, new TranslationTextComponent("container.usefulrailroads.track_builder.mode"), 169, 6, 0x404040);
+		font.func_243248_b(matrixStack, railsTextComponent, 8, 20, 0x404040);
+		font.func_243248_b(matrixStack, groundBlocksTextComponent, 8, 52, 0x404040);
+		font.func_243248_b(matrixStack, tunnelBlocksTextComponent, 8, 102, 0x404040);
+		font.func_243248_b(matrixStack, redstoneTorchesTextComponent, 8, 170, 0x404040);
+		font.func_243248_b(matrixStack, torchesTextComponent, 116, 170, 0x404040);
 		
-		font.func_243248_b(matrixStack, new TranslationTextComponent("container.usefulrailroads.track_builder.rails"), 8, 20, 0x404040);
-		font.func_243248_b(matrixStack, new TranslationTextComponent("container.usefulrailroads.track_builder.ground_blocks"), 8, 52, 0x404040);
-		font.func_243248_b(matrixStack, new TranslationTextComponent("container.usefulrailroads.track_builder.tunnel_blocks"), 8, 102, 0x404040);
-		font.func_243248_b(matrixStack, new TranslationTextComponent("container.usefulrailroads.track_builder.redstone_torches"), 8, 170, 0x404040);
-		font.func_243248_b(matrixStack, new TranslationTextComponent("container.usefulrailroads.track_builder.torches"), 116, 170, 0x404040);
+		final ITextComponent fuelComponent = fuelTextComponent.copyRaw().appendString(": ").append(new StringTextComponent(Integer.toString(container.getWrapper().getFuel())).mergeStyle(TextFormatting.DARK_AQUA));
 		
-		final String fuelString = I18n.format(langKey + "fuel") + ": " + TextFormatting.DARK_AQUA + container.getWrapper().getFuel();
-		
-		font.drawString(matrixStack, fuelString, xSize - font.getStringWidth(fuelString) - 6, 170, 0x404040);
+		font.func_243248_b(matrixStack, fuelComponent, xSize - font.getStringPropertyWidth(fuelComponent) - 6, 170, 0x404040);
 	}
 	
 }
