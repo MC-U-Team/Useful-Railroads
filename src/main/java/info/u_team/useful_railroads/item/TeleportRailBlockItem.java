7883e74a92844a1af9e195b6e7845138baa9b28a
HyCraftHD <hycrafthd@live.de>
2022-06-27 22:38:06 +0200

    Port item package

--- src/main/java/info/u_team/useful_railroads/item/TeleportRailBlockItem.java
+++ src/main/java/info/u_team/useful_railroads/item/TeleportRailBlockItem.java
@@ -1,70 +1,79 @@
 package info.u_team.useful_railroads.item;
 
+import java.util.Random;
+
 import info.u_team.u_team_core.util.MathUtil;
 import info.u_team.useful_railroads.block.TeleportRailBlock;
 import info.u_team.useful_railroads.util.Location;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.effect.LightningBoltEntity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.Rarity;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Util;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity.RemovalReason;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 public class TeleportRailBlockItem extends BlockItem {
 	
+	@Deprecated // TODO remove when uteamcore updates the random methods
+	private static Random random = new Random();
+	
 	public TeleportRailBlockItem(TeleportRailBlock block, Properties builder) {
-		super(block, builder.maxStackSize(1).rarity(Rarity.EPIC));
+		super(block, builder.stacksTo(1).rarity(Rarity.EPIC));
 	}
 	
 	@Override
-	public ActionResultType tryPlace(BlockItemUseContext context) {
-		final CompoundNBT compound = context.getItem().getChildTag("BlockEntityTag");
+	public InteractionResult place(BlockPlaceContext context) {
+		final CompoundTag compound = context.getItemInHand().getTagElement("BlockEntityTag");
 		if (compound != null && compound.contains("location")) {
-			return super.tryPlace(context);
+			return super.place(context);
 		}
-		final PlayerEntity player = context.getPlayer();
-		if (player != null && player.getEntityWorld().isRemote) {
-			player.sendMessage(new TranslationTextComponent("block.usefulrailroads.teleport_rail.missing_setup").mergeStyle(TextFormatting.RED), Util.DUMMY_UUID);
+		final Player player = context.getPlayer();
+		if (player != null && player.getCommandSenderWorld().isClientSide) {
+			player.sendSystemMessage(Component.translatable("block.usefulrailroads.teleport_rail.missing_setup").withStyle(ChatFormatting.RED));
 		}
-		return ActionResultType.FAIL;
+		return InteractionResult.FAIL;
 	}
 	
 	@Override
 	public boolean onEntityItemUpdate(ItemStack stack, ItemEntity itemEntity) {
-		final CompoundNBT compound = stack.getChildTag("BlockEntityTag");
+		final CompoundTag compound = stack.getTagElement("BlockEntityTag");
 		if (compound != null && compound.contains("location")) { // Prevent overwriting already installed rails
 			return false;
 		}
-		final World world = itemEntity.getEntityWorld();
-		final Vector3d itemEntityVector = itemEntity.getPositionVec();
-		if (world.isRemote) { // Do client particles
-			if (world.rand.nextInt(10) == 0) {
+		itemEntity.getLevel();
+		final Level world = itemEntity.getCommandSenderWorld();
+		final Vec3 itemEntityVector = itemEntity.position();
+		if (world.isClientSide) { // Do client particles
+			if (world.random.nextInt(10) == 0) {
 				for (int i = 0; i < 5; i++) {
-					world.addParticle(ParticleTypes.ENCHANT, true, itemEntityVector.getX(), itemEntityVector.getY() + 0.5, itemEntityVector.getZ(), MathUtil.randomNumberInRange(world.rand, -0.2, 0.2), MathUtil.randomNumberInRange(world.rand, 0.1, 1.5), MathUtil.randomNumberInRange(world.rand, -0.2, 0.2));
+					// world.addParticle(ParticleTypes.ENCHANT, true, itemEntityVector.x(), itemEntityVector.y() + 0.5,
+					// itemEntityVector.z(), MathUtil.randomNumberInRange(world.random, -0.2, 0.2),
+					// MathUtil.randomNumberInRange(world.random, 0.1, 1.5), MathUtil.randomNumberInRange(world.random, -0.2, 0.2));
+					world.addParticle(ParticleTypes.ENCHANT, true, itemEntityVector.x(), itemEntityVector.y() + 0.5, itemEntityVector.z(), MathUtil.randomNumberInRange(random, -0.2, 0.2), MathUtil.randomNumberInRange(random, 0.1, 1.5), MathUtil.randomNumberInRange(random, -0.2, 0.2));
 				}
 			}
 		} else { // Do server stuff
-			final int age = itemEntity.age;
+			final int age = itemEntity.getAge();
 			if (age < 100) { // Don't do anything when the age is not above 100 ticks
 				return false;
 			}
-			final AxisAlignedBB aabb = new AxisAlignedBB(itemEntityVector.getX() - 1, itemEntityVector.getY() - 1, itemEntityVector.getZ() - 1, itemEntityVector.getX() + 1, itemEntityVector.getY() + 1, itemEntityVector.getZ() + 1);
+			final AABB aabb = new AABB(itemEntityVector.x() - 1, itemEntityVector.y() - 1, itemEntityVector.z() - 1, itemEntityVector.x() + 1, itemEntityVector.y() + 1, itemEntityVector.z() + 1);
 			// Search all item entities in a range of 1 block away from the origin entity.
 			// Then lookup if it's an enderpearl.
-			world.getEntitiesInAABBexcluding(itemEntity, aabb, ItemEntity.class::isInstance).stream() //
+			world.getEntities(itemEntity, aabb, ItemEntity.class::isInstance).stream() //
 					.map(ItemEntity.class::cast) //
 					.map(ItemEntity::getItem) //
 					.filter(otherStack -> otherStack.getItem() == Items.ENDER_PEARL) //
@@ -74,22 +83,22 @@ public class TeleportRailBlockItem extends BlockItem {
 						otherStack.shrink(1);
 						
 						// Set location to the stack
-						stack.getOrCreateChildTag("BlockEntityTag").put("location", new Location(world.getDimensionKey(), itemEntity.getPosition()).serializeNBT());
+						stack.getOrCreateTagElement("BlockEntityTag").put("location", new Location(world.dimension(), itemEntity.blockPosition()).serializeNBT());
 						
-						final ItemEntity newItemEntity = new ItemEntity(world, itemEntityVector.getX(), itemEntityVector.getY(), itemEntityVector.getZ(), stack);
-						newItemEntity.setDefaultPickupDelay();
+						final ItemEntity newItemEntity = new ItemEntity(world, itemEntityVector.x(), itemEntityVector.y(), itemEntityVector.z(), stack);
+						newItemEntity.setDefaultPickUpDelay();
 						
 						// Delete old entity
-						itemEntity.remove();
+						itemEntity.remove(RemovalReason.DISCARDED);
 						
 						// Spawn new one with updated stack
-						world.addEntity(newItemEntity);
+						world.addFreshEntity(newItemEntity);
 						
-						if (world instanceof ServerWorld) {
-							final LightningBoltEntity lightningbolt = EntityType.LIGHTNING_BOLT.create(world);
-							lightningbolt.moveForced(itemEntityVector);
-							lightningbolt.setEffectOnly(true);
-							world.addEntity(lightningbolt);
+						if (world instanceof ServerLevel) {
+							final LightningBolt lightningbolt = EntityType.LIGHTNING_BOLT.create(world);
+							lightningbolt.moveTo(itemEntityVector);
+							lightningbolt.setVisualOnly(true);
+							world.addFreshEntity(lightningbolt);
 						}
 					});
 		}
--- src/main/java/info/u_team/useful_railroads/item/TrackBuilderItem.java
+++ src/main/java/info/u_team/useful_railroads/item/TrackBuilderItem.java
@@ -10,84 +10,81 @@ import info.u_team.useful_railroads.container.TrackBuilderContainer;
 import info.u_team.useful_railroads.init.UsefulRailroadsItemGroups;
 import info.u_team.useful_railroads.inventory.TrackBuilderInventoryWrapper;
 import info.u_team.useful_railroads.util.TrackBuilderManager;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.container.SimpleNamedContainerProvider;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
-import net.minecraft.item.Rarity;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.network.NetworkHooks;
 
 public class TrackBuilderItem extends UItem {
 	
 	private final boolean doubleTrack;
 	
 	public TrackBuilderItem(boolean doubleTrack) {
-		super(UsefulRailroadsItemGroups.GROUP, new Properties().maxStackSize(1).rarity(doubleTrack ? Rarity.EPIC : Rarity.RARE));
+		super(UsefulRailroadsItemGroups.GROUP, new Properties().stacksTo(1).rarity(doubleTrack ? Rarity.EPIC : Rarity.RARE));
 		this.doubleTrack = doubleTrack;
 	}
 	
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
-		final ItemStack stack = player.getHeldItem(hand);
-		if (!world.isRemote && !player.isSneaking() && player instanceof ServerPlayerEntity) { // Server & Player not sneaking
-			final TrackBuilderInventoryWrapper wrapper = new TrackBuilderInventoryWrapper.Server(stack, () -> player.world);
-			final int selectedSlot = hand == Hand.MAIN_HAND ? player.inventory.currentItem : -1;
+	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+		final ItemStack stack = player.getItemInHand(hand);
+		if (!level.isClientSide && !player.isShiftKeyDown() && player instanceof ServerPlayer serverPlayer) { // Server & Player not sneaking
+			final TrackBuilderInventoryWrapper wrapper = new TrackBuilderInventoryWrapper.Server(stack, () -> player.level);
+			final int selectedSlot = hand == InteractionHand.MAIN_HAND ? player.getInventory().selected : -1;
 			
-			NetworkHooks.openGui((ServerPlayerEntity) player, new SimpleNamedContainerProvider((id, playerInventory, openPlayer) -> {
+			NetworkHooks.openGui(serverPlayer, new SimpleMenuProvider((id, playerInventory, openPlayer) -> {
 				return new TrackBuilderContainer(id, playerInventory, wrapper, selectedSlot);
-			}, new TranslationTextComponent("container.usefulrailroads.track_builder")), buffer -> {
+			}, Component.translatable("container.usefulrailroads.track_builder")), buffer -> {
 				buffer.writeVarInt(wrapper.getFuel());
-				buffer.writeEnumValue(wrapper.getMode());
+				buffer.writeEnum(wrapper.getMode());
 				buffer.writeVarInt(selectedSlot);
 			});
 		}
-		return new ActionResult<>(ActionResultType.SUCCESS, stack);
+		return InteractionResultHolder.success(stack);
 	}
 	
 	@Override
-	public ActionResultType onItemUse(ItemUseContext context) {
-		final World world = context.getWorld();
-		if (world.isRemote) { // Server side
-			return ActionResultType.PASS;
+	public InteractionResult useOn(UseOnContext context) {
+		final Level level = context.getLevel();
+		if (level.isClientSide) { // Server side
+			return InteractionResult.PASS;
 		}
-		final PlayerEntity player = context.getPlayer();
-		if (player == null || !context.hasSecondaryUseForPlayer() || context.getHand() == Hand.OFF_HAND) { // No player or no sneaking and no offhand
-			return ActionResultType.PASS;
+		final Player player = context.getPlayer();
+		if (player == null || !context.isSecondaryUseActive() || context.getHand() == InteractionHand.OFF_HAND) { // No player or no sneaking and no offhand
+			return InteractionResult.PASS;
 		}
-		final TrackBuilderInventoryWrapper wrapper = new TrackBuilderInventoryWrapper.Server(context.getItem(), () -> player.world);
+		final TrackBuilderInventoryWrapper wrapper = new TrackBuilderInventoryWrapper.Server(context.getItemInHand(), () -> player.level);
 		
-		TrackBuilderManager.create(context.getPos(), context.getFace(), world, player.getLookVec(), wrapper.getMode(), doubleTrack).ifPresent(manager -> {
+		TrackBuilderManager.create(context.getClickedPos(), context.getClickedFace(), level, player.getLookAngle(), wrapper.getMode(), doubleTrack).ifPresent(manager -> {
 			manager.execute(player, wrapper);
 		});
-		return ActionResultType.SUCCESS;
+		return InteractionResult.SUCCESS;
 	}
 	
 	@Override
 	public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
-		return !ItemStack.areItemsEqual(oldStack, newStack);
+		return !ItemStack.isSame(oldStack, newStack);
 	}
 	
 	@Override
-	public CompoundNBT getShareTag(ItemStack stack) {
+	public CompoundTag getShareTag(ItemStack stack) {
 		if (ServerConfig.getInstance().shareAllNBTData.get()) {
 			return super.getShareTag(stack);
 		}
 		if (!stack.hasTag()) {
 			return null;
 		}
-		final CompoundNBT compound = stack.getTag().copy();
+		final CompoundTag compound = stack.getTag().copy();
 		compound.remove("Items");
 		if (compound.isEmpty()) {
 			return null;
@@ -96,19 +93,18 @@ public class TrackBuilderItem extends UItem {
 	}
 	
 	@Override
-	public boolean onDroppedByPlayer(ItemStack item, PlayerEntity player) {
-		return !(player.openContainer instanceof TrackBuilderContainer);
+	public boolean onDroppedByPlayer(ItemStack item, Player player) {
+		return !(player.containerMenu instanceof TrackBuilderContainer);
 	}
 	
 	public boolean isDoubleTrack() {
 		return doubleTrack;
 	}
 	
-	@OnlyIn(Dist.CLIENT)
 	@Override
-	public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
-		tooltip.add(TooltipCreator.create(this, "", 0, TooltipCreator.create(UsefulRailroadsMod.MODID, "click", "right_click", 0).mergeStyle(TextFormatting.ITALIC, TextFormatting.GOLD)).mergeStyle(TextFormatting.GRAY));
-		tooltip.add(TooltipCreator.create(this, "", 1, TooltipCreator.create(UsefulRailroadsMod.MODID, "click", "shift_right_click", 0).mergeStyle(TextFormatting.ITALIC, TextFormatting.GOLD)).mergeStyle(TextFormatting.GRAY));
+	public void appendHoverText(ItemStack stack, Level world, List<Component> tooltip, TooltipFlag flag) {
+		tooltip.add(TooltipCreator.create(this, "", 0, TooltipCreator.create(UsefulRailroadsMod.MODID, "click", "right_click", 0).withStyle(ChatFormatting.ITALIC, ChatFormatting.GOLD)).withStyle(ChatFormatting.GRAY));
+		tooltip.add(TooltipCreator.create(this, "", 1, TooltipCreator.create(UsefulRailroadsMod.MODID, "click", "shift_right_click", 0).withStyle(ChatFormatting.ITALIC, ChatFormatting.GOLD)).withStyle(ChatFormatting.GRAY));
 	}
 	
 }
