2763a4ffb423c7cba0f7d7bf472fce2566494bf7
HyCraftHD <hycrafthd@live.de>
2025-01-25 01:27:52 +0100

    Simplify recipe type with new uteamcore method

--- common/src/common/java/info/u_team/useful_railroads/init/UsefulRailroadsRecipeTypes.java
+++ common/src/common/java/info/u_team/useful_railroads/init/UsefulRailroadsRecipeTypes.java
@@ -2,6 +2,7 @@ package info.u_team.useful_railroads.init;
 
 import info.u_team.u_team_core.api.registry.CommonRegister;
 import info.u_team.u_team_core.api.registry.RegistryEntry;
+import info.u_team.u_team_core.recipetype.URecipeType;
 import info.u_team.useful_railroads.UsefulRailroadsReference;
 import info.u_team.useful_railroads.recipe.TeleportRailFuelRecipe;
 import info.u_team.useful_railroads.recipe.TrackBuilderFuelRecipe;
@@ -12,22 +13,8 @@ public class UsefulRailroadsRecipeTypes {
 	
 	public static final CommonRegister<RecipeType<?>> RECIPE_TYPES = CommonRegister.create(Registries.RECIPE_TYPE, UsefulRailroadsReference.MODID);
 	
-	public static final RegistryEntry<RecipeType<TeleportRailFuelRecipe>> TELEPORT_RAIL_FUEL = RECIPE_TYPES.register("teleport_rail_fuel", name -> new RecipeType<TeleportRailFuelRecipe>() {
-		
-		// TODO move maybe common recipe type creator to uteamcore?
-		@Override
-		public String toString() {
-			return name.toString();
-		}
-	});
-	public static final RegistryEntry<RecipeType<TrackBuilderFuelRecipe>> TRACK_BUILDER_FUEL = RECIPE_TYPES.register("track_builder_fuel", name -> new RecipeType<TrackBuilderFuelRecipe>() {
-		
-		// TODO move maybe common recipe type creator to uteamcore?
-		@Override
-		public String toString() {
-			return name.toString();
-		}
-	});
+	public static final RegistryEntry<RecipeType<TeleportRailFuelRecipe>> TELEPORT_RAIL_FUEL = RECIPE_TYPES.register("teleport_rail_fuel", URecipeType::new);
+	public static final RegistryEntry<RecipeType<TrackBuilderFuelRecipe>> TRACK_BUILDER_FUEL = RECIPE_TYPES.register("track_builder_fuel", URecipeType::new);
 	
 	static void register() {
 		RECIPE_TYPES.register();
