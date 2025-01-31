12adb1290347df98d024db659d6729261f4e8bd8
HyCraftHD <hycrafthd@live.de>
2025-01-31 14:34:41 +0100

    Add direction implementation again

--- neoforge/src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsNeoForgeCapabilities.java
+++ neoforge/src/main/java/info/u_team/useful_railroads/init/UsefulRailroadsNeoForgeCapabilities.java
@@ -4,6 +4,7 @@ import info.u_team.u_team_core.util.CastUtil;
 import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_railroads.blockentity.NeoForgeBufferStopBlockEntity;
 import info.u_team.useful_railroads.blockentity.NeoForgeTeleportRailBlockEntity;
+import net.minecraft.core.Direction;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
@@ -11,11 +12,17 @@ public class UsefulRailroadsNeoForgeCapabilities {
 	
 	private static void registerCapabilities(RegisterCapabilitiesEvent event) {
 		event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, UsefulRailroadsBlockEntityTypes.TELEPORT_RAIL.get(), (blockEntity, context) -> {
-			return CastUtil.assertCast(blockEntity, NeoForgeTeleportRailBlockEntity.class).getFuelSlotWrapper();
+			if (context != Direction.UP) {
+				return CastUtil.assertCast(blockEntity, NeoForgeTeleportRailBlockEntity.class).getFuelSlotWrapper();
+			}
+			return null;
 		});
 		
 		event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, UsefulRailroadsBlockEntityTypes.BUFFER_STOP.get(), (blockEntity, context) -> {
-			return CastUtil.assertCast(blockEntity, NeoForgeBufferStopBlockEntity.class).getMinecartSlotsWrapper();
+			if (context == Direction.DOWN || context == null) {
+				return CastUtil.assertCast(blockEntity, NeoForgeBufferStopBlockEntity.class).getMinecartSlotsWrapper();
+			}
+			return null;
 		});
 	}
 	
